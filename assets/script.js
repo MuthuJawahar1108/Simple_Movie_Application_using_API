03696a6cd9baa5dbf7ae3e1f902405788e9f2445
MuthuJawahar1108 <kvmjawa20031108@gmail.com>
2023-10-30 08:11:27 +0530

    Almoost completed HTML

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,13 @@
 
 const API_URL = "http://www.omdbapi.com?apikey=d6ae06e7";
 
-const searchMovies = async(title) => {
-    const response = await fetch(`${API_URL}&s=`)
 
+const searchMovies = async(title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    console.log(data.Search);
 }
+
+searchMovies("Spiderman");
